9c313851e9bd6f1802b74d8e1f32bc18534a54d9
imalie <imaldewaas@gmail.com>
2020-01-26 19:51:42 +0530

    remove folder

--- Pro_Tracker_ima_Ui_Logo/controllers/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-//DBMS Connection settings
-$dbServer = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbDatabase = "pro_tracker";
-
-//DBMS Connection create
-$conn = mysqli_connect($dbServer, $dbUser, $dbPassword, $dbDatabase);
